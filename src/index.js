53781f2cb0beed4f50aeba330f284834ae68474b
Liam Tinney <ltinney@powerhrg.com>
2020-08-12 14:31:03 -0400

    Add Validation w/ Custom Errors

--- src/index.js
+++ src/index.js
@@ -3,11 +3,39 @@ import ReactDOM from "react-dom";
 import { useFormik } from "formik";
 import "./styles.css";
 
+// A custom validation function. This must return an object
+ // which keys are symmetrical to our values/initialValues
+ const validate = values => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = 'Required';
+  } else if (values.firstName.length > 15) {
+    errors.firstName = 'Must be 15 characters or less';
+  }
+
+  if (!values.lastName) {
+    errors.lastName = 'Required';
+  } else if (values.lastName.length > 20) {
+    errors.lastName = 'Must be 20 characters or less';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  return errors;
+};
+
+
 const SignupForm = () => {
   // Pass the useFormik() hook initial form values and a submit function that will
   // be called when the form is submitted
   const formik = useFormik({
     initialValues: {
+      firstName: '',
+      lastName: '',
       email: '',
     },
     onSubmit: values => {
@@ -16,6 +44,22 @@ const SignupForm = () => {
   });
   return (
     <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+      id="firstName"
+      name="firstName"
+      type="text"
+      onChange={formik.handleChange}
+      value={formik.values.firstName}
+      />
+        <label htmlFor="lastName">Last Name</label>
+        <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
